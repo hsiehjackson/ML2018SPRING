79c41ad3636384101ed6271c19edaff28175a732
hsiehjackson <37269846+hsiehjackson@users.noreply.github.com>
2018-05-21 04:33:20 -0700

    Add files via upload

--- hw5/hw5_test.sh
+++ hw5/hw5_test.sh
@@ -1,5 +1,5 @@
 wget -O word2vec.h5.trainables.syn1neg.npy 'https://www.dropbox.com/s/krzyckvgbow005t/word2vec.h5.trainables.syn1neg.npy?dl=1'
 wget -O word2vec.h5.wv.vectors.npy 'https://www.dropbox.com/s/ycsd5nku10twych/word2vec.h5.wv.vectors.npy?dl=1'
-wget -O public 'https://www.dropbox.com/s/4b0qcgau960985n/my_ensemble_model.h5?dl=1' 
-wget -O private 'https://www.dropbox.com/s/4b0qcgau960985n/my_ensemble_model.h5?dl=1' 
+wget -O public 'https://www.dropbox.com/s/vuqbeq1oy81odbg/0.83375model.h5?dl=1' 
+wget -O private 'https://www.dropbox.com/s/vuqbeq1oy81odbg/0.83375model.h5?dl=1' 
 python3 testhw5.py $1 $2 $3
